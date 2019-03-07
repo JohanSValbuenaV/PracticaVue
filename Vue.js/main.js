2a4ec008dd0c2271acdcd6ff6b4254c3e6215ffd
Johan S Valbuena <u1201847@unimilitar.edu.co>
2019-03-07 09:41:52 -0500

    Attribute Binding

--- Vue.js/main.js
+++ Vue.js/main.js
@@ -1,7 +1,9 @@
 var app = new Vue({
     el: '#app',
-    data:{
-        product: 'Socks',description: "warm"
+    data: {
+        product: 'Socks',
+        image: './imagenes/vmSocks-green-onWhite.jpg',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
     }
-    
+
 })
