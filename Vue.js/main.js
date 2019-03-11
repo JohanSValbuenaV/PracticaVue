09846c353bbe7db378c73b18872a3b925ea0e7fc
Johan S Valbuena <u1201847@unimilitar.edu.co>
2019-03-11 14:49:41 -0500

    List Rendering

--- Vue.js/main.js
+++ Vue.js/main.js
@@ -4,8 +4,19 @@ var app = new Vue({
         product: 'Socks',
         image: './imagenes/vmSocks-green-onWhite.jpg',
         inStock: true,
-        inventory: 100,
-        onSale: true
+        details: ["80% cotton", "20% polyester", "gender neutral"],
+
+        variants: [{
+                variantId: 2234,
+                VariantColor: "green"
+            },
+            {
+                variantId: 2235,
+                VariantColor: "blue"
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+
     }
 
 })
