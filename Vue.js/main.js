73fb070a02a71905ecb7de995e84727c33d9fd2e
Johan S Valbuena <u1201847@unimilitar.edu.co>
2019-03-16 10:21:33 -0500

    computed properties

--- Vue.js/main.js
+++ Vue.js/main.js
@@ -1,35 +1,58 @@
 var app = new Vue({
     el: '#app',
     data: {
+
+        brand: 'Vue Mastery',
         product: 'Socks',
-        image: './imagenes/vmSocks-green-onWhite.jpg',
-        inStock: false,
+        selectedVariant:0,
+
         details: ["80% cotton", "20% polyester", "gender neutral"],
 
         variants: [{
                 variantId: 2234,
                 VariantColor: "green",
-                variantimage: './imagenes/vmSocks-green-onWhite.jpg'
+                variantimage: './imagenes/vmSocks-green-onWhite.jpg',
+                variantquantity: 10
             },
             {
                 variantId: 2235,
                 VariantColor: "blue",
-                variantimage: './imagenes/vmSocks-blue-onWhite.jpg'
+                variantimage: './imagenes/vmSocks-blue-onWhite.jpg',
+                variantquantity: 0
             }
         ],
 
-        cart: 0
+        cart: 0,
+        onSale: true
     },
     methods: {
         addToCart: function () {
             this.cart += 1;
         },
-        updateProduct: function (variantimage) {
-            this.image = variantimage;
+        updateProduct: function (index) {
+            this.selectedVariant = index;
+    
         },
         removeFromCart() {
             this.cart -= 1;
         }
+    },
+    computed: {
+        titulo(){
+            return this.brand + ' ' +this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantimage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantquantity
+        },
+        sale() {
+            if (this.onSale) {
+              return this.brand + ' ' + this.product + ' are on sale!'
+            } 
+              return  this.brand + ' ' + this.product + ' are not on sale'
+          }
     }
 
 })
