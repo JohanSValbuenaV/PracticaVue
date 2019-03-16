4c9005fba2daa9051dd8f5cfc52302defdfd32d3
Johan S Valbuena <u1201847@unimilitar.edu.co>
2019-03-16 10:47:12 -0500

    components

--- Vue.js/main.js
+++ Vue.js/main.js
@@ -1,29 +1,84 @@
-var app = new Vue({
-    el: '#app',
-    data: {
+Vue.component('product-details', {
+    props: {
+      details: {
+        type: Array,
+        required: true
+      }
+    },
+    template: `
+      <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+      </ul>
+    `
+  })
+  
 
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        selectedVariant:0,
+Vue.component('product', {
+props:{
+    premium:{
+        type: Boolean,
+        required: true
+    }
+},
+    template: `
+    <div class="product">
+<div class="product-image">
+    <img :src="image" alt="">
+</div>
+<div class="product-info">
+    <h1>{{titulo}}</h1>
+    <p v-if="inStock">In Stock</p>
+    <p v-else :class="{ outOfStock: !inStock }">Out Stock</p>
+    <p>Shipping: {{shipping}} </p>
 
-        details: ["80% cotton", "20% polyester", "gender neutral"],
+    <p>{{sale}}</p>
 
-        variants: [{
-                variantId: 2234,
-                VariantColor: "green",
-                variantimage: './imagenes/vmSocks-green-onWhite.jpg',
-                variantquantity: 10
-            },
-            {
-                variantId: 2235,
-                VariantColor: "blue",
-                variantimage: './imagenes/vmSocks-blue-onWhite.jpg',
-                variantquantity: 0
-            }
-        ],
+    <product-details :details="details"></product-details>
 
-        cart: 0,
-        onSale: true
+
+    <div v-for="(variant, index) in variants" :key="variant.VariantId" class="color-box" :style="{backgroundColor: variant.VariantColor}"
+        @mouseover="updateProduct(index)">
+
+    </div>
+
+    <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock}">
+        Add to Cart</button>
+    <button @click="removeFromCart">Remove from cart</button>
+
+    <div class="cart">
+        <p>Cart({{cart}})</p>
+    </div>
+
+</div>
+
+
+</div>`,
+
+    data() {
+        return {
+            brand: 'Vue Mastery',
+            product: 'Socks',
+            selectedVariant: 0,
+
+            details: ["80% cotton", "20% polyester", "gender neutral"],
+
+            variants: [{
+                    variantId: 2234,
+                    VariantColor: "green",
+                    variantimage: './imagenes/vmSocks-green-onWhite.jpg',
+                    variantquantity: 10
+                },
+                {
+                    variantId: 2235,
+                    VariantColor: "blue",
+                    variantimage: './imagenes/vmSocks-blue-onWhite.jpg',
+                    variantquantity: 0
+                }
+            ],
+
+            cart: 0,
+            onSale: true
+        }
     },
     methods: {
         addToCart: function () {
@@ -31,28 +86,43 @@ var app = new Vue({
         },
         updateProduct: function (index) {
             this.selectedVariant = index;
-    
+
         },
         removeFromCart() {
             this.cart -= 1;
         }
     },
     computed: {
-        titulo(){
-            return this.brand + ' ' +this.product
+        titulo() {
+            return this.brand + ' ' + this.product
         },
-        image(){
+        image() {
             return this.variants[this.selectedVariant].variantimage
         },
-        inStock(){
+        inStock() {
             return this.variants[this.selectedVariant].variantquantity
         },
         sale() {
             if (this.onSale) {
-              return this.brand + ' ' + this.product + ' are on sale!'
-            } 
-              return  this.brand + ' ' + this.product + ' are not on sale'
-          }
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+            return this.brand + ' ' + this.product + ' are not on sale'
+        },
+        shipping(){
+            if(this.premium){
+                return  "Free"
+            }
+            return "2.99"
+        }
     }
 
+
+})
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
+    }
 })
